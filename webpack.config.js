6f80771ddd777c0f06673ad8c5c9331015b37435
ShineShao <xiaoshaoqq@gmail.com>
2022-12-16 22:47:45 +0800

    remove: wasm

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+// const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, argv) => {
@@ -9,47 +9,47 @@ module.exports = (env, argv) => {
     devServer: {
       contentBase: distPath,
       compress: isProd,
-      port: 8000
+      port: 8000,
     },
     entry: "./app/index.ts",
     output: {
       path: distPath,
       filename: "index.js",
-      webassemblyModuleFilename: "index.wasm"
+      // webassemblyModuleFilename: "index.wasm",
     },
     module: {
       rules: [
         {
           test: /\.s[ac]ss$/i,
-          use: ["style-loader", "css-loader", "sass-loader"]
+          use: ["style-loader", "css-loader", "sass-loader"],
         },
         {
           test: /\.(ts|js)?$/,
           use: "ts-loader",
-          exclude: /node_modules/
+          exclude: /node_modules/,
         },
         {
           test: /\.(png|jpg|gif|jpeg)$/,
-          loader: "url-loader"
-        }
-      ]
+          loader: "url-loader",
+        },
+      ],
     },
     plugins: [
       new CopyWebpackPlugin({
-        patterns: [{ from: "./public", to: distPath }]
+        patterns: [{ from: "./public", to: distPath }],
       }),
       // We point our WasmPackPlugin to the location of the
       // the crates `Cargo.toml` file. Never the root file.
-      new WasmPackPlugin({
-        crateDirectory: path.join(__dirname, "wasm"),
-        outDir: path.join(__dirname, "wasm/pkg")
-        // extraArgs: ""
-      })
+      // new WasmPackPlugin({
+      //   crateDirectory: path.join(__dirname, "wasm"),
+      //   outDir: path.join(__dirname, "wasm/pkg")
+      //   // extraArgs: ""
+      // })
     ],
     watch: argv.mode !== "production",
     resolve: {
       // A little overkill for our tutorial but useful.
-      extensions: [".ts", ".tsx", ".js", ".jsx", ".mts", ".mjs"]
-    }
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".mts", ".mjs"],
+    },
   };
 };
